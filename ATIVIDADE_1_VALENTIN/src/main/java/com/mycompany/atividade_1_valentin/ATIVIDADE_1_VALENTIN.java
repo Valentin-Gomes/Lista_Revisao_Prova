59dbff92095a914a67bd89bbe66c0cc1a5dac434
Valentin-Gomes <valentin_gomes@hotmail.com>
2024-10-23 18:47:17 -0300

    Preparando Git

---
+++ ATIVIDADE_1_VALENTIN/src/main/java/com/mycompany/atividade_1_valentin/ATIVIDADE_1_VALENTIN.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.atividade_1_valentin;
+
+/**
+ *
+ * @author v.gomes
+ */
+public class ATIVIDADE_1_VALENTIN {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
